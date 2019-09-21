aab7dccc1105cca845280d0514a87cbf449c29ec
Alefe Cruz <alefecrz@gmail.com>
2019-09-21 19:06:03 -0300

    Build back-end.

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-const express = require('express');
-const port = process.env.PORT || 3000;
+const express = require('express')
+const port = process.env.PORT || 3000
 
-const app = express();
+const app = express()
+
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/controllers'));
+app.use(express.static(__dirname + '/node_modules'));
 
 app.get('/', (req, res)=>{
-    res.send("Welcome the Game Hash");
+    res.sendfile('./views/gameView.html');
 })
+
 
 app.listen(port, 
     function (){ 
-        console.log("Running in port 3000."); 
+        console.log("Running in port 3000.");
     } 
-);
+)
